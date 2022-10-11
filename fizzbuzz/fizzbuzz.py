4c556ec831ec353564d36a02af7bdfd9da35f31d
SomeStupidSongBird <563ftw@gmail.com>
2022-10-11 19:33:07 -0400

    making pylint happy

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,40 +1,41 @@
-import click
+"""it's fizzbuzz, what do you expect"""
 import sys
+import click
 
 
-def is_positive_integer(n):
+def is_positive_integer(number):
     """Returns True if n is a positive integer"""
-    return n>0
+    return number>0
 
 
-def process(n):
+def process(number):
     """Returns the string representation of integer n unless:
        - n is evenly divisible by 3 (returns 'Fizz')
        - n is evenly divisible by 5 (returns 'Buzz')
        - n is evenly divisible by both 3 and 5 (returns 'FizzBuzz')"""
-    outString = ""
-    if n%3==0:
-        outString+="Fizz"
-    if n%5==0:
-        outString+="Buzz"
-    if outString=="":
-        outString = str(n)
-    return outString
+    out_string = ""
+    if number%3==0:
+        out_string+="Fizz"
+    if number%5==0:
+        out_string+="Buzz"
+    if out_string=="":
+        out_string = str(number)
+    return out_string
 
 
 @click.command()
 @click.option('-n', default=100, help='A positive integer')
-def main(n):
-
+def main(number):
+    """it's the main function, what do you expect?"""
     # Validate input
-    if not is_positive_integer(n):
+    if not is_positive_integer(number):
         print("the value of n must be a positive, non-zero integer")
         sys.exit()
 
     # Iterate from 1 to n inclusive and print FizzBuzz
-    for i in range(1, n+1):
+    for i in range(1, number+1):
         print(process(i))
 
 
 if __name__ == '__main__':
-    main()
+    main(15)
